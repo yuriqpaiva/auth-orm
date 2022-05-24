e21bc4444283d22a2c6afae3334a7487917f1263
Yuri Paiva <yuriqpaiva@gmail.com>
2022-05-24 20:40:15 -0300

    documenting application

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@ const dotenv = require('dotenv');
 const routes = require('./routes');
 require('./auth/passports');
 require('./auth/redis/blocklistAccessToken');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('../swagger.json');
 dotenv.config();
 
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 routes(app);
 
 const port = process.env.APP_PORT;
